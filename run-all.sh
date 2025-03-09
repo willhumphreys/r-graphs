a4e3cfca9489fbe929b8013e7f8a952675527291
will <whumphreys@gmail.com>
2025-03-09 23:29:39 +0100

    Add packages and update file reference in Docker and script
    Added 'Matrix' and 'reshape' to the R packages installed in the Dockerfile for extended functionality. Updated the hardcoded aggregated file path in `run-all.sh` to align with the current directory structure.

--- run-all.sh
+++ run-all.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Hardcoded aggregated trades file
-AGGREGATED_FILE="es-1mF/s_-3000..-100..400___l_100..7500..400___o_-800..800..100___d_14..14..7___out_8..8..4___short___mw___wc=9/aggregated-es-1mF_s_-3000..-100..400___l_100..7500..400___o_-800..800..100___d_14..14..7___out_8..8..4___short___mw___wc=9_aggregationQueryTemplate-all.csv.lzo"
+AGGREGATED_FILE="es-1mF/s_-3000..-100..400___l_100..7500..400___o_-800..800..100___d_14..14..7___out_8..8..4/aggregated-es-1mF_s_-3000..-100..400___l_100..7500..400___o_-800..800..100___d_14..14..7___out_8..8..4_aggregationQueryTemplate-all.csv.lzo"
 
 # Define the R scripts to run
 declare -a SCRIPTS=("src/bestTraders.r" "src/stops.r" "src/years.r")
