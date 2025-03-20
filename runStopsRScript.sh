659181bb085ba567d1acab5578ddf199a1b4264a
will <whumphreys@gmail.com>
2025-03-20 23:19:02 +0100

    Use environment variables for S3 buckets in script
    Replace hardcoded S3 bucket names with environment variables, falling back to default values if not set. This adds flexibility and makes the script easier to configure for different environments.

--- runStopsRScript.sh
+++ runStopsRScript.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -e
+
+# Use environment variables with defaults as fallback
+S3_SOURCE_BUCKET=${SOURCE_BUCKET:-"mochi-trades-aggregated"}
+S3_GRAPHS_BUCKET=${GRAPHS_BUCKET:-"mochi-graphs"}
+
 find . -name \*.lzo -delete
 find . -name \*.csv -delete
 echo "Copy from S3 with ${1}"
 dest="$(echo $1 | awk -F/ '{print $NF}')"
 echo "Destination is ${dest}"
-aws s3 cp s3://mochi-trades-aggregated/$1 ${dest}
+aws s3 cp s3://${S3_SOURCE_BUCKET}/$1 ${dest}
 echo "File copied from S3. Decompressing"
 lzop -d ${dest}
 file=$(basename "${dest}" .lzo)
@@ -17,4 +22,4 @@ Rscript $2 ${file} results
 #echo "Execute years.r"
 #Rscript years.r ${file} results
 #echo "Sync graphs to S3"
-aws s3 sync results s3://mochi-graphs/$1
+aws s3 sync results s3://${S3_GRAPHS_BUCKET}/$1
